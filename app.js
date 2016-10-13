5bfbad9670ae4f3be5e421add4669825e90089ee
Jakub Sikora <jakub.sikora@pure360.com>
2016-10-13 11:15:35 +0100

    working with axios

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
 import './assets/css/app.less';
+import axios from 'axios';
 
-console.log('foo5');
+axios({
+  method: 'post',
+  url: 'https://api.travis-ci.com/auth/github',
+  data: {
+    github_token: 'xxx'
+  },
+  headers: {
+    Accept: 'application/vnd.travis-ci.2+json'
+  },
+})
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+// var $ = require('jquery');
+
+// $.ajax({
+//   method: 'post',
+//   url: 'https://api.travis-ci.com/auth/github',
+//   headers: {
+//     Accept: 'application/vnd.travis-ci.2+json'
+//   },
+//   data: { github_token: 'xxx' },
+//   dataType: 'json',
+//   success: function (data) {
+//     const access_token = data.access_token;
+//     console.log(access_token);
+
+//     // setInterval(function(){
+//     //   $.ajax({
+//     //     method: 'get',
+//     //     url: 'https://api.travis-ci.com/repos/Pure360/frontend/builds',
+//     //     headers: {
+//     //       Accept: 'application/vnd.travis-ci.2+json',
+//     //       Authorization: 'token ' + access_token
+//     //     },
+//     //     dataType: 'json',
+//     //     success: function (data) {
+//     //       console.info(data.builds[0].state);
+//     //     }
+//     //   });
+//     // }, 5000);
+//   }
+// });
