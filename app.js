958453aea6e961b377cdfbea0e48b3c9f3431460
Jakub Sikora <jakub.sikora@pure360.com>
2016-10-14 09:47:58 +0100

    wip

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import './assets/css/app.less';
 import Bulb from './src/bulb';
+import Travis from './src/travis';
+import config from './config';
+
+console.log(config);
 
 const bulb = new Bulb();
+const travis = new Travis();
 
 function setEventHandlers() {
   const btnConnect = document.querySelector('.btn-connect');
--- src/travis.js
+++ src/travis.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios({
-  method: 'post',
-  url: 'https://api.travis-ci.com/auth/github',
-  data: {
-    github_token: 'xxx'
-  },
-  headers: {
-    Accept: 'application/vnd.travis-ci.2+json'
-  },
-})
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// axios({
+//   method: 'post',
+//   url: 'https://api.travis-ci.com/auth/github',
+//   data: {
+//     github_token: 'xxx'
+//   },
+//   headers: {
+//     Accept: 'application/vnd.travis-ci.2+json'
+//   },
+// })
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
 // var $ = require('jquery');
 
 // $.ajax({
@@ -46,3 +46,12 @@ axios({
 //     // }, 5000);
 //   }
 // });
+
+
+class Travis {
+  init() {
+
+  }
+}
+
+export default Travis;
